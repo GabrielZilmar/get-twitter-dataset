4ab79264975e7cce38cc1d7feb9291c8cd73a88c
GabrielZilmar <gabrielzilmar@hotmail.com>
2021-04-21 11:14:09 -0300

    feat: get tweets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const readCsv = require('./app/readCsv');
+const getTweet = require('./app/getTweet');
 
 const app = async () => {
-	console.log(await readCsv());
+	const dataset = await readCsv();
+	const comments = [];
+	for (let i = 0; i < dataset.length; i += 1) {
+		comments.push(await getTweet(dataset[i].TweetID));
+	}
+	console.log(comments);
 };
 
 app();
